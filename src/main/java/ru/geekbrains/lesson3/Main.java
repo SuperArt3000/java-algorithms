cb95ed06684a2d18440c7da77610865d14f8bc6e
Nika <zurbaevi.nika@gmail.com>
2020-10-27 21:20:43 +0400

    Optimized code

--- src/main/java/ru/geekbrains/lesson3/Main.java
+++ src/main/java/ru/geekbrains/lesson3/Main.java
@@ -20,61 +20,63 @@ public class Main {
         Реализуйте итератор на основе связанных списков из задания 3.4 и выполните базовые операции итератора.
         Оцените время выполнения операций с помощью базового метода System.nanoTime() */
 
-        long start;
+        long lastTime;
 
         Integer[] arr = {9, 8, 2, 1, 5, 6, 4, 3, 10, 7};
-        System.out.println("Task 3.1.");
-        start = System.nanoTime();
+        System.out.println("Task 3.1.\nStarting array: " + Arrays.toString(arr));
+        lastTime = System.nanoTime();
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(arr));
-        System.out.println("ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(arr)): " + (System.nanoTime() - start) + " nanosecond \n");
+        System.out.println("Getting the arrayList: " + arrayList);
+        System.out.println("The execution time of converting an array to a list took: " + (System.nanoTime() - lastTime) + " nanosecond\n");
 
-        System.out.println("Task 3.2.");
-        start = System.nanoTime();
+        System.out.println("Task 3.2.\nAdding element:");
+        lastTime = System.nanoTime();
         arrayList.add(10, 100);
-        System.out.println("arrayList.add(10, 100): " + (System.nanoTime() - start) + " nanosecond");
-        start = System.nanoTime();
+        System.out.println("The resulting arrayList after adding an element: " + arrayList);
+        System.out.println("The execution time of the method for adding an item to the list took: " + (System.nanoTime() - lastTime) + " nanosecond");
+        System.out.println("Deleting element:");
+        lastTime = System.nanoTime();
         arrayList.remove(10);
-        System.out.println("arrayList.remove(10): " + (System.nanoTime() - start) + " nanosecond");
-        start = System.nanoTime();
-        System.out.println("arrayList.get(8) result " + arrayList.get(8) + " took: " + (System.nanoTime() - start) + " nanosecond\n");
+        System.out.println("The resulting arrayList after removing the element: " + arrayList);
+        System.out.println("The execution time of the method for removing an item from the list took: " + (System.nanoTime() - lastTime) + " nanosecond");
+        lastTime = System.nanoTime();
+        System.out.println("Received arrayList element: " + arrayList.get(8));
+        System.out.println("The execution time of the method to find an item from the list took: " + (System.nanoTime() - lastTime) + " nanosecond\n");
 
         System.out.println("Task 3.3.");
         LinkedList<Integer> list = new LinkedList<>();
         for (int i = 0; i < 3; i++) list.insert(i);
-        System.out.print("list.display(): ");
+        System.out.print("The resulting singly linked list: ");
         list.display();
-        System.out.println("\nlist.find(2): " + list.find(2) + "\n");
+        System.out.println("\nAn example of finding the desired element using the find method: " + list.find(2) + "\n");
 
         System.out.print("Task 3.4.");
         java.util.LinkedList<Integer> linkedList = new java.util.LinkedList<>();
         linkedList.addAll(0, arrayList);
-        System.out.print("\nlinkedList.addAll(0, arrayList): " + linkedList + "\n");
         linkedList.add(100);
-        System.out.print("linkedList.add(100): " + linkedList + "\n");
+        System.out.println("\nAdding an item to a two-sided list: " + linkedList);
         linkedList.set(4, 500);
-        System.out.print("linkedList.set(4, 500): " + linkedList + "\n");
+        System.out.println("Replacing an item in a two-sided list: " + linkedList);
         linkedList.remove(10);
-        System.out.print("linkedList.remove(10): " + linkedList + "\n");
+        System.out.println("Remove an item from a two-sided list: " + linkedList);
         linkedList.addFirst(100);
         linkedList.addLast(200);
-        System.out.println("linkedList.addFirst(100) && linkedList.addLast(200): " + linkedList + "\n");
+        System.out.println("Adding the first and last item: " + linkedList + "\n");
 
-        System.out.println("Task 3.5.");
-        System.out.print("hasNext: ");
+        System.out.print("Task 3.5.\nCheck if the linked list contains the next element hasNext method: ");
         ListIterator<Integer> iterator = linkedList.listIterator();
-        start = System.nanoTime();
+        lastTime = System.nanoTime();
         while (iterator.hasNext()) System.out.print(iterator.next() + " ");
-        System.out.println(": " + (System.nanoTime() - start) + " nanosecond");
-        System.out.print("hasPrevious: ");
-        start = System.nanoTime();
+        System.out.println("\nThe execution time of these methods took: " + (System.nanoTime() - lastTime) + " nanosecond");
+        System.out.print("Check if the linked list contains the previous element hasPrevious method: ");
+        lastTime = System.nanoTime();
         while (iterator.hasPrevious()) System.out.print(iterator.previous() + " ");
-        System.out.print(": " + (System.nanoTime() - start) + " nanosecond");
-        System.out.print("\nRemoving list items: ");
-        start = System.nanoTime();
+        System.out.println("\nThe execution time of these methods took: " + (System.nanoTime() - lastTime) + " nanosecond");
+        lastTime = System.nanoTime();
         while (iterator.hasNext()) {
             iterator.next();
             iterator.remove();
         }
-        System.out.println((System.nanoTime() - start) + " nanosecond");
+        System.out.println("Execution time deleting data took: " + (System.nanoTime() - lastTime) + " nanosecond");
     }
 }
